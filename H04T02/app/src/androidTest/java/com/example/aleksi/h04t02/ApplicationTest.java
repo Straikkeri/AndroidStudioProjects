c3384217e7a17404dd5118a009b6ee9db30bcd48
Aleksi Tommila <aleksi.tommila@gmail.com>
2016-10-03 21:03:20 +0300

    H04T02 added screenshots, again

---
+++ H04T02/app/src/androidTest/java/com/example/aleksi/h04t02/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.aleksi.h04t02;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ H04T02/app/src/main/java/com/example/aleksi/h04t02/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.aleksi.h04t02;
+
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listview;
+    private String mediaPath;
+    private List<String> songs = new ArrayList<String>(); // string lista biisien nimille
+    private MediaPlayer mediaPlayer = new MediaPlayer();
+    private LoadSongsTask task;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listview = (ListView) findViewById(R.id.listView);
+        mediaPath = Environment.getExternalStorageDirectory().getPath() + "/Music/";
+
+        //lisätään listviewille onlcick kuuntelija, ja määritellään sille mediaplayerin alustus ja starttikomennot try catchin sisään(?) mikä on adapterview?
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                try {
+                    mediaPlayer.reset();
+                    mediaPlayer.setDataSource(songs.get(position));
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch(IOException e) {
+                    Toast.makeText(getBaseContext(), "Cannot start audio!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        task = new LoadSongsTask();
+        task.execute();
+
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mediaPlayer.isPlaying()) mediaPlayer.reset();
+    }
+
+    private class LoadSongsTask extends AsyncTask<Void, String, Void> {
+
+        private List<String> loadedSongs = new ArrayList<String>();
+
+        //UI-threadi
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(getApplicationContext(),"Loading...",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        //Taustathreadi
+        @Override
+        protected Void doInBackground(Void... url) {
+            updateSongListRecursive(new File(mediaPath));
+            return null;
+        }
+
+        //Edelleen taustathreadi //kutsuu itseään for-loopissa?
+        public void updateSongListRecursive(File path) {
+            if (path.isDirectory()) {
+                for (int i = 0; i < path.listFiles().length; i++) {
+                    File file = path.listFiles()[i];
+                    updateSongListRecursive(file);
+                }
+
+            } else {
+                String name = path.getAbsolutePath();
+                publishProgress(name);
+                if (name.endsWith(".mp3")) {
+                    loadedSongs.add(name);
+                }
+            }
+        }
+
+        //UI-threadi
+        @Override
+        protected void onPostExecute(Void args) {
+            ArrayAdapter<String> songList = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, loadedSongs);
+            listview.setAdapter(songList);
+            songs = loadedSongs;
+
+            Toast.makeText(getApplicationContext(), "Songs="+songs.size(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
